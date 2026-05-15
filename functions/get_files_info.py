c4714965803151673a4395c82e3242544efd17fe
Matt <mgstankov30@gmail.com>
2026-05-15 12:32:22 -0400

    add more details for these test files to show the results of the functional run including error outputs and file sizes

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,12 +7,24 @@ def get_files_info(working_directory, directory="."):
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
     if not valid_target_dir:
-      return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+      return f'Result for current directory:\nError: Cannot list "{directory}" as it is outside the permitted working directory'
     
     if not os.path.isdir(target_dir):
       return f'Error: "{directory}" is not a directory'
     
     if valid_target_dir:
-      return f'Success: "{directory}" is within the working directory'
+      print(f'Success: "{directory}" is within the working directory')
+      directory_contents = os.listdir(target_dir)
+      try:
+        directory_strings = []
+        for directory in directory_contents:
+          full_path = os.path.join(target_dir, directory)
+          directory_strings.append(f"- {directory}:  file_size={os.path.getsize(full_path)} bytes, is_dir={os.path.isdir(full_path)}")
+      except:
+        return "Error: failed to fet path size or validate diretory"
+      directory_details = "\n".join(directory_strings)
+      print(f"Result for current directory:\n{directory_details}")
   except:
     return f'Error: The standard library functionality failed and returned an error.'
+  
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -22,6 +22,7 @@ from functions.get_files_info import get_files_info
 #     unittest.main()
 
 print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "pkg"))
 print(get_files_info("calculator", "/bin"))
 print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
+# print(get_files_info("calculator", "main.py"))
