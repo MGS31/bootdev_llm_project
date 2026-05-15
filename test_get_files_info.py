ec868f8deb3f58207dc8e9296958df9779b95873
Matt <mgstankov30@gmail.com>
2026-05-15 15:05:08 -0400

    add additional file for write_file in functions, update main to be cleaner, update test_get_files_info to be cleaner, add lorem.txt for testing get file content. Add functiion to get file_content and test file to test this functionality

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,28 +1,24 @@
-# import unittest
 from functions.get_files_info import get_files_info
 
-# class TestGetFilesInfo(unittest.TestCase):
-#   def test_getInfo_calculator(self):
-#     result = get_files_info("calculator", ".")
-#     self.assertEqual(result, 'Success: "." is within the working directory')
 
-#   def test_getInfo_calculator_bin(self):
-#     result = get_files_info("calculator", "/bin")
-#     self.assertEqual(result, 'Error: Cannot list "/bin" as it is outside the permitted working directory')
-  
-#   def test_getInfo_calulator_sub(self):
-#     result = get_files_info("calculator", "../")
-#     self.assertEqual(result, 'Error: Cannot list "../" as it is outside the permitted working directory')
-  
-#   def test_getInfo_calculator_main(self):
-#     result = get_files_info("calculator", "main.py")
-#     self.assertEqual(result, 'Error: "main.py" is not a directory')
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
 
-# if __name__ == "__main__":
-#     unittest.main()
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
-# print(get_files_info("calculator", "main.py"))
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
