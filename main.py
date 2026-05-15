92b6be9113538fcb5b9cd685bc99333eb4825399
Matt <mgstankov30@gmail.com>
2026-05-15 09:42:39 -0400

    add verobse option to provide more details when calling the LLM

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -25,9 +26,12 @@ def main():
     else:
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokes = response.usage_metadata.candidates_token_count
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokes}")
-    print("Response: ", response.text)
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokes}")
+        print("Response: ", response.text)
+    else:
+        print("Response: ", response.text)
 
 
 if __name__ == "__main__":
